3338f44460f60a34a2325c32eb97e3e9951e39c8
dastakhov <dastakhov@ptsecurity.com>
2020-05-06 00:32:14 +0000

    [activity] Add grow calculations methods

--- src/user_config/activity.go
+++ src/user_config/activity.go
@@ -9,12 +9,44 @@ type activity struct {
 }
 
 func (act activity) IsActual(startTime time.Time, currentTime time.Time) bool {
+	passedInMetric := act.passedInMetric(startTime, currentTime)
+	result := (passedInMetric + act.Periodicity.Denominator) % act.Periodicity.Addendum
+	return !(result == 0)
+}
+
+func (act activity) GetSummary(startTime time.Time, currentTime time.Time) bool {
+	passedInMetric := act.passedInMetric(startTime, currentTime)
+	result := (passedInMetric + act.Periodicity.Denominator) % act.Periodicity.Addendum
+	return !(result == 0)
+}
+
+func (act activity) getGrow(passedInMetric int) int {
+	switch act.Grow.GrowFunction.Type {
+	case "monotonous":
+		return act.getMonotonousGrow(passedInMetric)
+	default:
+		return act.getConstGrow(passedInMetric)
+	}
+}
+
+func (act activity) getConstGrow(passedInMetric int) int {
+	return act.Grow.Borders.LeftBorder
+}
+
+func (act activity) getMonotonousGrow(passedInMetric int) int {
+	expected := act.Grow.Borders.LeftBorder + act.Grow.GrowFunction.Coefficient*passedInMetric
+	if expected > act.Grow.Borders.RightBorder {
+		return act.Grow.Borders.RightBorder
+	}
+	return expected
+}
+
+func (act activity) passedInMetric(startTime time.Time, currentTime time.Time) int {
 	passedInMetric := int(currentTime.Sub(startTime).Hours())
 	if act.Periodicity.Metrics == "day" {
 		passedInMetric = int(passedInMetric / 24)
 	}
-	result := (passedInMetric + act.Periodicity.Denominator) % act.Periodicity.Addendum
-	return !(result == 0)
+	return passedInMetric
 }
 
 type grow struct {
