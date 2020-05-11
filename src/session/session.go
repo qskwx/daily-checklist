1a605765f784e7f0d4cbdabab5a4ba0d40774185
qskwx <thekopkin@gmail.com>
2020-05-11 01:09:21 +0000

    [session] Implement session fabric and activity completion methods

--- src/session/session.go
+++ src/session/session.go
@@ -2,25 +2,35 @@ package session
 
 import (
 	userconfig "daily-checklist/src/user_config"
+	"time"
 )
 
 type session struct {
 	user       userconfig.UserConfig
-	categories []category
+	categories categories
 }
 
-type category struct {
-	name        string
-	prefix      string
-	acitivities []activity
+// TODO: check username as input
+
+func sessionFabric(username string) session {
+	user, _ := userconfig.UserConfigFabric(username)
+	ss := session{
+		user:       user,
+		categories: categoriesFabric(user.GetCurrentActivities(time.Now()))}
+	return ss
 }
 
-type activity struct {
-	id       string
-	activity string
-	done     bool
+func (ss *session) setDone(actID string) error {
+	if err := ss.categories.setDone(actID); err != nil {
+		return err
+	}
+	return nil
 }
 
 func IsExpired() int {
 	return 0
+}
+
+func toCharStr(i int) string {
+	return string('a' + i)
 }
