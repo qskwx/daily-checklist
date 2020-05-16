8302a6f75a5c1655d1c3d4f8fcc3c2e23d561b5b
qskwx <thekopkin@gmail.com>
2020-05-16 21:48:47 +0000

    [session][category] Add getters/setters

--- src/session/category.go
+++ src/session/category.go
@@ -2,14 +2,26 @@ package session
 
 type category struct {
 	name       string
-	prefix     string
 	activities activities
+	prefix     string
 }
 
 func categoryFabric(name string, prefix string, acts []string) category {
 	return category{
 		name:       name,
-		prefix:     prefix,
 		activities: activitiesFabric(prefix, acts),
+		prefix:     prefix,
 	}
+}
+
+func (cat category) Name() string {
+	return cat.name
+}
+
+func (cat category) Activities() activities {
+	return cat.activities
+}
+
+func (cat *category) setDone(activityId string) error {
+	return cat.activities.setDone(activityId)
 }
