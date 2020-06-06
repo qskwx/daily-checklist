fb5800ce692cc3d19f2e827abfb5cd83d56f2cc9
qskwx <thekopkin@gmail.com>
2020-06-06 21:39:57 +0000

    Move main to cmd dir

--- cmd/daily-bot/main.go
+++ cmd/daily-bot/main.go
@@ -4,8 +4,6 @@ import (
 	bot "daily-checklist/src/interface"
 )
 
-const telegramAPIKey string = "875847790:AAEKNbIJ-U8X99I1JTpgAuDF15ZSqy9m8DI"
-
 func main() {
 	bot, _ := bot.BotFactory(telegramAPIKey)
 	bot.Loop()
